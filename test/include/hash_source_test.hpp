e09ca4bd509ba2c21dcb028dabdab52ebab9031a
Michael Vesterli <michael@vesterli.dk>
2019-08-10 14:01:48 +0200

    Reduce failures in hash_source_test
    FHTCrossPolytope is restricted in the rotations it can perform, which
    means that for some vectors, some bits are unused.
    That is not what this test is about, so the number of vectors used is
    increased.

--- test/include/hash_source_test.hpp
+++ test/include/hash_source_test.hpp
@@ -32,19 +32,27 @@ namespace hash_source {
         unsigned int num_hashes,
         unsigned int hash_length
     ) {
-        auto vec = UnitVectorFormat::generate_random(dimensions.args);
-        auto stored = to_stored_type<typename T::Sim::Format>(vec, dimensions);
-        auto state = source->reset(stored.get());
-        uint64_t max_hash = (((1 << (hash_length-1))-1) << 1)+1;
-
         std::vector<int> bit_occurences(hash_length, 0);
+        std::vector<std::unique_ptr<Hash>> hash_functions;
         for (unsigned int i=0; i < num_hashes; i++) {
-            auto hasher = source->sample();
-            uint64_t hash = (*hasher)(state.get());
-            REQUIRE(hash <= max_hash);
-            for (unsigned int bit=0; bit < hash_length; bit++) {
-                if (hash & (1ull << bit)) {
-                    bit_occurences[bit]++;
+            hash_functions.push_back(source->sample());
+        }
+
+        // Test with a couple of different vectors since the limited range of some hash functions
+        // (such as FHTCrossPolytope) can lead to some bits unused.
+        for (int vec_idx = 0; vec_idx < 2; vec_idx++) {
+            auto vec = UnitVectorFormat::generate_random(dimensions.args);
+            auto stored = to_stored_type<typename T::Sim::Format>(vec, dimensions);
+            auto state = source->reset(stored.get());
+            uint64_t max_hash = (((1 << (hash_length-1))-1) << 1)+1;
+
+            for (unsigned int i=0; i < num_hashes; i++) {
+                uint64_t hash = (*hash_functions[i])(state.get());
+                REQUIRE(hash <= max_hash);
+                for (unsigned int bit=0; bit < hash_length; bit++) {
+                    if (hash & (1ull << bit)) {
+                        bit_occurences[bit]++;
+                    }
                 }
             }
         }
@@ -89,28 +97,30 @@ namespace hash_source {
 
     TEST_CASE("HashPool hashes") {
         const unsigned int HASH_LENGTH = 24;
+        unsigned int samples = 100;
         Dataset<UnitVectorFormat> dataset(100);
         auto dimensions = dataset.get_description();
         test_hashes<SimHash>(
             dimensions,
-            HashPoolArgs<SimHash>(60).build(dimensions, 0, HASH_LENGTH),
-            100,
+            HashPoolArgs<SimHash>(60).build(dimensions, samples, HASH_LENGTH),
+            samples,
             HASH_LENGTH);
         test_hashes<FHTCrossPolytopeHash>(
             dimensions,
-            HashPoolArgs<FHTCrossPolytopeHash>(60).build(dimensions, 0, HASH_LENGTH),
-            100,
+            HashPoolArgs<FHTCrossPolytopeHash>(60).build(dimensions, samples, HASH_LENGTH),
+            samples,
             HASH_LENGTH);
     }
 
     TEST_CASE("HashPool sketches") {
         const unsigned int HASH_LENGTH = 64;
+        unsigned int samples = 100;
         Dataset<UnitVectorFormat> dataset(100);
         auto dimensions = dataset.get_description();
         test_hashes<SimHash>(
             dimensions,
-            HashPoolArgs<SimHash>(60).build(dimensions, 0, HASH_LENGTH),
-            100,
+            HashPoolArgs<SimHash>(60).build(dimensions, samples, HASH_LENGTH),
+            samples,
             HASH_LENGTH);
     }
 
