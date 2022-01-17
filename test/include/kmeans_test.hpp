259a515a65026bdec7e5a92500020ec7f0259899
Viktor Bello Thomsen <viktor.b.thomsen@gmail.com>
2022-01-17 16:19:45 +0100

    Added kmeans test 2

--- test/include/kmeans_test.hpp
+++ test/include/kmeans_test.hpp
@@ -7,7 +7,7 @@
 
 #include <vector>
 #include <iostream>
-
+#include <set>
 
 using namespace puffinn;
 namespace kmeans {
@@ -35,4 +35,46 @@ namespace kmeans {
         REQUIRE(is_correct);
     }
 
+    TEST_CASE("basic kmeans clustering 2") {
+        unsigned int N = 18, dims = 3, K=3;
+        std::vector<float>  data[N] = { {2.0 , 2.0 , 0.0},
+                                        {4.0 , 4.0 , 0.0},
+                                        {2.0 , 4.0 , 0.0},
+                                        {4.0 , 2.0 , 0.0},
+                                        {3.0 , 3.0 , -1.0},
+                                        {3.0 , 3.0 , 1.0},
+                                        {2.0 , 0.0 , 2.0},
+                                        {4.0 , 0.0 , 4.0},
+                                        {2.0 , 0.0 , 4.0},
+                                        {4.0 , 0.0 , 2.0},
+                                        {3.0 , 1.0 , 3.0},
+                                        {3.0 , -1.0 , 3.0},
+                                        {0.0 , 2.0 , 2.0},
+                                        {0.0 , 4.0 , 4.0},
+                                        {0.0 , 2.0 , 4.0},
+                                        {0.0 , 4.0 , 2.0},
+                                        {1.0 , 3.0 , 3.0},
+                                        {-1.0 , 3.0 , 3.0}};
+
+        Dataset<RealVectorFormat> dataset(dims, N);
+        for (auto entry : data) {
+            dataset.insert(entry);
+        }
+        std::cout << "Tim du tager fucking fejl" << std::endl;
+        KMeans<RealVectorFormat> kmeans(dataset, (uint8_t)K);
+        kmeans.fit();
+        float   *cen1 = kmeans.getCentroid(0),
+                *cen2 = kmeans.getCentroid(1),
+                *cen3 = kmeans.getCentroid(2);
+        std::vector<float> c1(cen1, cen1+3);
+        std::vector<float> c2(cen2, cen2+3);
+        std::vector<float> c3(cen3, cen3+3);
+        std::set<std::vector<float>>
+                                correct = {{0.0, 3.0, 3.0},
+                                            {3.0, 0.0, 3.0},
+                                            {3.0,3.0, 0.0}}, 
+                                centroids = {c1,c2,c3};
+            
+        REQUIRE(correct == centroids);
+    }
 }
