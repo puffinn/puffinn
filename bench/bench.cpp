f49395861125d4aa94adafcb325a050a83cdc176
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2022-02-15 12:16:03 +0100

    Add simple benchmark

---
+++ bench/bench.cpp
@@ -0,0 +1,89 @@
+#include <nanobench.h>
+
+#include "puffinn/collection.hpp"
+#include "puffinn/hash/simhash.hpp"
+#include "puffinn/hash/crosspolytope.hpp"
+#include "puffinn/hash_source/pool.hpp"
+#include "puffinn/hash_source/independent.hpp"
+#include "puffinn/hash_source/tensor.hpp"
+#include "puffinn/similarity_measure/cosine.hpp"
+#include "puffinn/similarity_measure/jaccard.hpp"
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <chrono>
+
+const unsigned int MB = 1024*1024;
+
+std::vector<std::vector<float>> read_glove(const std::string& filename);
+
+int main(int argc, char ** argv) {
+    if (argc != 2) {
+        std::cerr << "USAGE: Bench <FILE>" << std::endl;
+        return 1;
+    }
+    printf("Reading vectors\n");
+    auto dataset = read_glove(argv[1]);
+    printf("Vectors read\n");
+
+    auto dimensions = dataset[0].size(); 
+
+    // To benchmark the index build time we have to start from a new
+    // index at each measurement iteration, otherwise the index is already populated
+    // and no rebuild is triggered.
+    auto bencher = ankerl::nanobench::Bench()
+        .title("Index building")
+        .minEpochIterations(10)
+        .timeUnit(std::chrono::milliseconds(1), "ms");
+    auto index_memory = 100*MB;
+
+    // Therefore, first we bench how much time it takes to push things into the index
+    bencher.run("index_insert_data", [&] {
+        puffinn::Index<puffinn::CosineSimilarity> index(
+            dimensions,
+            index_memory
+        );
+        for (auto v : dataset) { index.insert(v); }
+    });
+    
+    // Therefore, first we bench how much time it takes to push things into the index
+    bencher.run("index_rebuild", [&] {
+        puffinn::Index<puffinn::CosineSimilarity> index(
+            dimensions,
+            index_memory
+        );
+        for (auto v : dataset) { index.insert(v); }
+        index.rebuild();
+    });
+}
+
+std::vector<std::vector<float>> read_glove(const std::string& filename) {
+    std::ifstream file(filename);
+
+    if (!file.is_open()) {
+        throw std::invalid_argument("File not found");
+    }
+
+    std::vector<std::vector<float>> vectors;
+    while (!file.eof()) {
+        std::string full_line;
+        std::getline(file, full_line);
+        std::istringstream line(full_line);
+        
+        std::string word;
+        line >> word;
+
+        std::vector<float> row;
+        float val;
+        while (line >> val) {
+            row.push_back(val);
+        }
+
+        if (row.size() != 0) {
+            vectors.push_back(row);
+        }
+    }
+
+    return vectors;
+}
