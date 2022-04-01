8789cb383258b97224963cbc4ea84d0884009907
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2022-04-01 10:45:06 +0200

    Start work on experimental harness

--- join-experiments/puffinn-join.cpp
+++ join-experiments/puffinn-join.cpp
@@ -6,116 +6,116 @@
 #include <vector>
 #include <highfive/H5Attribute.hpp>
 #include <highfive/H5File.hpp>
-#include <highfive/H5Easy.hpp>
 #include "puffinn.hpp"
 #include "puffinn/performance.hpp"
 
-template <typename DataFormat>
-std::vector<DataFormat> load_dataset(char * hdf5_path);
+const unsigned long long MB = 1024*1024;
 
-template <>
-std::vector<std::vector<uint32_t>> load_dataset(char * hdf5_path) {
-    HighFive::File file(hdf5_path, HighFive::File::ReadOnly);
-    std::vector<uint32_t> items = H5Easy::load<std::vector<uint32_t>>(file, "vectors/items");
-    std::vector<uint32_t> offsets = H5Easy::load<std::vector<uint32_t>>(file, "vectors/offsets");
-    std::vector<uint32_t> lengths = H5Easy::load<std::vector<uint32_t>>(file, "vectors/lengths");
+void expect(std::string what) {
+    std::cerr << "[c++] Expecting to receive `sppv1 " << what << "`" << std::endl;
+    std::string protocol_line;
+    std::getline(std::cin, protocol_line);
+    std::cerr << "[c++] received`" << protocol_line << "`" << std::endl;
+    if (protocol_line != "sppv1 " + what) {
+        std::cout << "sppv1 err" << std::endl;
+        throw "invalid message received";
+    }
+}
 
-    std::vector<std::vector<uint32_t>> vectors;
-    for (size_t i = 0; i < offsets.size(); i++) {
-        auto start = items.begin() + offsets[i];
-        auto end = start + lengths[i];
-        std::vector<uint32_t> v(start, end);
-        vectors.push_back(v);
+void send(std::string what) {
+    std::cout << "sppv1 " << what << std::endl;
+}
+
+std::vector<std::vector<float>> read_vectors_stdin() {
+    std::vector<std::vector<float>> vectors;
+    // Read vectors from standard input
+    std::string full_line;
+    while(std::getline(std::cin, full_line)) {
+        if (full_line.size() == 0) {
+            break;
+        }
+        
+        std::vector<float> row;
+        std::istringstream line(full_line);
+        float val;
+        while (line >> val) {
+            row.push_back(val);
+        }
+        vectors.push_back(row);
     }
     return vectors;
 }
 
-template <>
-std::vector<std::vector<float>> load_dataset(char * hdf5_path) {
-    HighFive::File file(hdf5_path, HighFive::File::ReadOnly);
-    std::vector<std::vector<float>> vectors = H5Easy::load<std::vector<std::vector<float>>>(file, "vectors");
-    return vectors;
-}
+int main(void) {
+    std::string protocol_line;
 
-std::string get_type(char * hdf5_path) {
-    HighFive::File file(hdf5_path, HighFive::File::ReadOnly);
-    std::string type;
-    file.getAttribute("type").read(type);
-    return type;
-}
+    // Read parameters
+    expect("setup");
+    std::cerr << "[c++] setup" << std::endl;
+    unsigned int k = 10;
+    float recall = 0.8;
+    std::string method = "BF";
+    unsigned long long space_usage = 100*MB;
+    while (true) {
+        std::getline(std::cin, protocol_line);
+        std::cerr << "[c++] setup line: " << protocol_line << std::endl;
+        if (protocol_line == "sppv1 end") {
+            break;
+        }
+        std::istringstream line(protocol_line);
+        std::string key;
+        line >> key;
+        if (key == "k") {
+            line >> k;
+        } else if (key == "recall") {
+            line >> recall;
+        } else if (key == "method") {
+            line >> method;
+        } else if (key == "space_usage") {
+            line >> space_usage;
+            space_usage *= MB;
+        } else {
+            std::cout << "sppv1 err unknown parameter " << key << std::endl;
+            return -1;
+        }
+    }
+    std::cerr << " method " << method << std::endl
+              << " k "  << k << std::endl
+              << " recall " << recall << std::endl
+              << " space_usage " << space_usage << std::endl;
+    send("ok");
 
-template<typename DataFormat>
-size_t get_dimensions(char * hdf5_path);
+    // Read the dataset
+    expect("data");
+    std::cerr << "[c++] receiving data" << std::endl;
+    auto dataset = read_vectors_stdin();
+    auto dimensions = dataset[0].size(); 
+    send("ok");
 
-template<>
-size_t get_dimensions<std::vector<uint32_t>>(char * hdf5_path) {
-    HighFive::File file(hdf5_path, HighFive::File::ReadOnly);
-    size_t universe;
-    file.getAttribute("universe").read(&universe);
-    puffinn::Dataset<puffinn::SetFormat> dataset(universe);
-    return universe;
-}
-
-template<>
-size_t get_dimensions<std::vector<float>>(char * hdf5_path) {
-    HighFive::File file(hdf5_path, HighFive::File::ReadOnly);
-    auto dimensions_attr = file.getAttribute("dimensions");
-    size_t dimensions;
-    dimensions_attr.read(&dimensions);
-    puffinn::Dataset<puffinn::UnitVectorFormat> dataset(dimensions);
-    return dimensions;
-}
-
-void print_stats() {
-    auto total_time =  puffinn::g_performance_metrics.get_total_time(puffinn::Computation::Total);
-    auto search_time = puffinn::g_performance_metrics.get_total_time(puffinn::Computation::Search);
-    auto filter_time = puffinn::g_performance_metrics.get_total_time(puffinn::Computation::Filtering);
-    auto init_time = puffinn::g_performance_metrics.get_total_time(puffinn::Computation::SearchInit);
-    auto indexing_time = puffinn::g_performance_metrics.get_total_time(puffinn::Computation::Indexing);
-    auto rebuild_time = puffinn::g_performance_metrics.get_total_time(puffinn::Computation::Rebuilding);
-    auto sorting_time = puffinn::g_performance_metrics.get_total_time(puffinn::Computation::Sorting);
-    auto index_hashing_time = puffinn::g_performance_metrics.get_total_time(puffinn::Computation::IndexHashing);
-    auto index_sketching_time = puffinn::g_performance_metrics.get_total_time(puffinn::Computation::IndexSketching);
-    std::cerr
-        << "indexing_time=" << indexing_time
-        << "\n\tsketching_time=" << index_sketching_time
-        << "\n\thashing_time=" << index_hashing_time
-        << "\n\trebuilding_time=" << rebuild_time
-        << "\n\tsorting_time" << sorting_time
-        << "\nsearch_time=" <<  search_time
-        << "\nfilter_time=" << filter_time
-        << "\ninit_time=" << init_time 
-        << "\ntotal_time=" << total_time << std::endl;
-}
-
-template <typename DataFormat, typename Similarity, typename HashFunction>
-void run(char * filename, uint32_t k, float recall, std::string method, size_t space_usage) {
-    auto start_time = std::chrono::steady_clock::now();
-    auto dataset = load_dataset<DataFormat>(filename);
-    size_t dimensions = get_dimensions<DataFormat>(filename);
-    auto end_time = std::chrono::steady_clock::now();
-    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
-    std::cerr << "Data loaded in " << elapsed << " ms" << std::endl;
-
+    expect("index");
     // Construct the search index.
-    puffinn::Index<Similarity, HashFunction> index(
+    // Here we use the cosine similarity measure with the default hash functions.
+    // The index expects vectors with the same dimensionality as the first row of the dataset
+    // and will use at most the specified amount of memory.
+    puffinn::Index<puffinn::CosineSimilarity, puffinn::SimHash> index(
         dimensions,
         space_usage,
-        puffinn::TensoredHashArgs<HashFunction>()
+        // puffinn::TensoredHashArgs<puffinn::SimHash>()
+        puffinn::IndependentHashArgs<puffinn::SimHash>()
     );
     // Insert each vector into the index.
-    for (auto v : dataset) {
-        index.insert(v);
-    }
-    start_time = std::chrono::steady_clock::now();
+    for (auto v : dataset) { index.insert(v); }
+    auto start_time = std::chrono::steady_clock::now();
     std::cerr << "Building the index. This can take a while..." << std::endl; 
     // Rebuild the index to include the inserted points
     index.rebuild(false);
-    end_time = std::chrono::steady_clock::now();
-    elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
-    auto throughput = ((float) dataset.size()) / (elapsed / 1000.0);
-    std::cerr << "Index built in " << elapsed << " ms " << throughput << " vecs/s" << std::endl;
+    auto end_time = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed = (end_time - start_time);
+    auto throughput = ((float) dataset.size()) / elapsed.count();
+    std::cerr << "Index built in " << elapsed.count() << " s " << throughput << " vecs/s" << std::endl;
+    send("ok");
 
+    expect("workload");
     start_time = std::chrono::steady_clock::now();
     std::cerr << "Computing the join using " << method << ". This can take a while." << std::endl;    
     std::vector<std::vector<uint32_t>>  res;
@@ -128,55 +128,56 @@ void run(char * filename, uint32_t k, float recall, std::string method, size_t s
     } else if (method == "LSHJoin") {
         res = index.lsh_join(k, recall);
     } else if (method == "LSHJoinGlobal") {
-        index.global_lsh_join(k, recall);
+        auto pairs = index.global_lsh_join(k, recall);
+        for (auto entry : pairs.best_indices()) {
+            std::vector<uint32_t> vpair;
+            vpair.push_back(entry.first);
+            vpair.push_back(entry.second);
+            res.push_back(vpair);
+        }
     }
     end_time = std::chrono::steady_clock::now();
-    auto elapsed_join = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
-    throughput = ((float) dataset.size()) / (elapsed_join / 1000.0);
-    std::cerr << "Join computed in " << elapsed_join << " ms " << throughput << " queries/s" << std::endl;
+    std::chrono::duration<double> elapsed_join = (end_time - start_time);
+    throughput = ((float) dataset.size()) / elapsed_join.count();
+    std::cerr << "Join computed in " << elapsed_join.count() << " s " << throughput << " queries/s" << std::endl;
+    send("ok");
 
-    print_stats();
+    expect("result");
+    std::cerr << "[c++] results size " << res.size() << std::endl; 
+    for (auto v : res) {
+        for (auto i : v) {
+            std::cout << i << " ";
+        }
+        std::cout << std::endl;
+    }
+    send("end");
+
+    // auto total_time =  puffinn::g_performance_metrics.get_total_time(puffinn::Computation::Total);
+    // auto search_time = puffinn::g_performance_metrics.get_total_time(puffinn::Computation::Search);
+    // auto filter_time = puffinn::g_performance_metrics.get_total_time(puffinn::Computation::Filtering);
+    // auto init_time = puffinn::g_performance_metrics.get_total_time(puffinn::Computation::SearchInit);
+    // auto indexing_time = puffinn::g_performance_metrics.get_total_time(puffinn::Computation::Indexing);
+    // auto rebuild_time = puffinn::g_performance_metrics.get_total_time(puffinn::Computation::Rebuilding);
+    // auto sorting_time = puffinn::g_performance_metrics.get_total_time(puffinn::Computation::Sorting);
+    // auto index_hashing_time = puffinn::g_performance_metrics.get_total_time(puffinn::Computation::IndexHashing);
+    // auto index_sketching_time = puffinn::g_performance_metrics.get_total_time(puffinn::Computation::IndexSketching);
+
+    // std::stringstream ss;
+
+    // ss << "search_time=" <<  search_time
+    //     << "; filter_time=" << filter_time
+    //     << "; init_time=" << init_time 
+    //     << "; total_time=" << total_time;
+
+    // std::cout 
+    //     << "indexing_time=" << indexing_time
+    //     << "\n\tsketching_time=" << index_sketching_time
+    //     << "\n\thashing_time=" << index_hashing_time
+    //     << "\n\trebuilding_time=" << rebuild_time
+    //     << "\n\tsorting_time" << sorting_time
+    //     << "\nsearch_time=" <<  search_time
+    //     << "\nfilter_time=" << filter_time
+    //     << "\ninit_time=" << init_time 
+    //     << "\ntotal_time=" << total_time << std::endl;
 
 }
-
-const unsigned long long MB = 1024*1024;
-
-// Takes the following arguments: filename (num_neighbors) (recall) (space_usage in MB)
-// The recall is a lower bound on the probability of finding each of the closest neighbors and is
-// between 0 and 1.
-int main(int argc, char* argv[]) {
-    if (argc != 6) {
-        printf("USAGE: PuffinnJoin filename k recall method space_usage");
-        return 1;
-    }
-    // Read parameters
-    char * filename = argv[1];
-    uint32_t k = atoi(argv[2]);
-    float recall = atof(argv[3]);
-    std::string method = argv[4];
-    size_t space_usage = atoi(argv[5])*MB;
-
-    auto dataset_type = get_type(filename);
-
-    if (dataset_type == "dense") {
-        run<
-            std::vector<float>,
-            puffinn::CosineSimilarity, 
-            puffinn::SimHash
-        >(filename, k, recall, method, space_usage);
-    } else if (dataset_type == "sparse") {
-        run<
-            std::vector<uint32_t>,
-            puffinn::JaccardSimilarity,
-            puffinn::MinHash1Bit
-        >(filename, k, recall, method, space_usage);
-    } else {
-        printf("Unknown dataset type %s\n", dataset_type.c_str());
-        return 1;
-    }
-
-
-    return 0;
-}
-
-
---
+++ join-experiments/run.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python3
+
+# This script handles the execution of the join experiments 
+# in two different modes:
+#  - global top-k
+#  - local top-k
+# 
+# Datasets are taken from ann-benchmarks or created ad-hoc from 
+# other sources (e.g. DBLP).
+
+import time
+import subprocess
+import h5py
+import sys
+import yaml
+import shlex
+
+# Communication protocol
+# ======================
+#
+# For algorithms interfacing over text streams, the protocol is as follows:
+#
+# - Setup phase
+#   - harness sends `sppv1 setup`, followed by parameters as `name value` pairs, followed by `sppv1 end`
+#   - program acknowledges using `sppv1 ok`
+# - Data ingestion
+#   - harness sends `sppv1 data`, followed by vectors, one per line, followed by `sppv1 end`
+#   - program acknowledges using `sppv1 ok`
+# - Index construction (timed)
+#   - harness sends `sppv1 index`
+#   - program acknowledges using `sppv1 ok` when it's done
+# - Workload run (timed)
+#   - harness sends `sppv1 workload`
+#   - program acknowledges using `sppv1 ok` when it's done
+# - Result collection
+#   - harness sends `sppv1 result`
+#   - program writes output on stdout, one item per line (for whatever "item" means for the workload)
+#   - program writes `sppv1 end`
+
+PROTOCOL = "sppv1" # Version of the communication protocol
+
+
+def text_encode_floats(v):
+    return " ".join(map(str, v))
+
+
+def h5cat(path, dataset, stream=sys.stdout):
+    file = h5py.File(path, "r")
+    for v in file[dataset]:
+        stream.write(text_encode_floats(v) + "\n")
+        stream.flush()
+        # print(text_encode_floats(v), file=stream)
+    print(file=stream) # Signal end of streaming
+
+
+class Algorithm(object):
+    """Manages the lifecycle of an algorithm"""
+    def execute(self, h5py_path, dataset):
+        self.setup()
+        self.feed_data(h5py_path, dataset)
+        self.index()
+        self.run()
+        self.result()
+        return self.times()
+
+    def setup(self):
+        """Configure the parameters of the algorithm"""
+        pass
+    def feed_data(self, h5py_path, dataset):
+        """Pass the data to the algorithm"""
+        pass
+    def index(self):
+        """Setup the index, if any. This is timed."""
+        pass
+    def run(self):
+        """Run the workload. This is timed."""
+        pass
+    def result(self):
+        """Collect the result"""
+        pass
+    def times(self):
+        """Returns the pair (index_time, workload_time)"""
+        pass
+    
+
+class GlobalTopKResult(object):
+    def __init__(self):
+        self.pairs = []
+
+    def add(self, i, j):
+        self.pairs.append((min(i, j), max(i, j)))
+
+    def add_line(self, line):
+        i, j = line.split()
+        self.add(int(i), int(j))
+
+
+class SubprocessAlgorithm(Algorithm):
+    """Manages the lifecycle of an algorithm which does not provide
+    a Python interface"""
+
+    def _raw_line(self):
+        return shlex.split(
+            self._subprocess_handle().stdout.readline().strip())
+
+    def _line(self):
+        line = self._raw_line()
+        while len(line) < 1 or line[0] != PROTOCOL:
+            line = self._raw_line()
+        return line[1:]
+
+    def _send(self, msg):
+        program = self._subprocess_handle()
+        print(PROTOCOL, msg, file=program.stdin)
+
+    def _expect(self, what, errmsg):
+        assert self._line()[0] == what, errmsg
+
+    # param result_collector: object that accepts lines with a method `add_line`,
+    # one by one, and converts them to at global or local top-k result
+    def __init__(self, command_w_args, params_dict, result_collector):
+        self.command_w_args = command_w_args
+        self._program = None
+        self.params_dict = params_dict
+        self.result_collector = result_collector
+        self.index_time = None
+        self.workload_time = None
+
+    def _subprocess_handle(self):
+        if self._program is None:
+            self._program = subprocess.Popen(
+                self.command_w_args,
+                bufsize=1,  # line buffering
+                stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+                universal_newlines=True)
+        return self._program
+
+    def setup(self):
+        print("Setup")
+        self._send("setup")
+        program = self._subprocess_handle()
+        for k, v in self.params_dict.items():
+            print(k, v, file=program.stdin)
+        self._send("end")
+        self._expect("ok", "setup failed")
+        
+    def feed_data(self, h5py_path, dataset):
+        print("Feeding data using pipes")
+        self._send("data")
+        program = self._subprocess_handle()
+        h5cat(h5py_path, dataset, program.stdin)
+        self._expect("ok", "population phase failed")
+
+    def index(self):
+        print("Building index")
+        start_t = time.time()
+        self._send("index")
+        self._expect("ok", "workload phase failed")
+        end_t = time.time()
+        self.index_time = end_t - start_t
+
+    def run(self):
+        print("Running workload")
+        start_t = time.time()
+        self._send("workload")
+        # Wait for the program to report success
+        self._expect("ok", "workload phase failed")
+        end_t = time.time()
+        self.run_time = end_t - start_t
+
+    def result(self):
+        print("Running collecting result")
+        self._send("result")
+        while True:
+            line = self._raw_line()
+            if line[1] == "end":
+                break
+            self.result_collector.add_line(" ".join(line))
+
+    def times(self):
+        return self.index_time, self.workload_time
+
+
+if __name__ == "__main__":
+    res = GlobalTopKResult()
+    algo = SubprocessAlgorithm(
+        ["build/GloveJoin"],
+        {"k": 10, "recall": 0.01, "method": "LSHJoinGlobal", "space_usage": 512},
+        res
+    )
+    time_index, time_workload = algo.execute("/tmp/glove.hdf5", "/test")
+    for pair in res.pairs:
+        print("[result pair]", pair)
