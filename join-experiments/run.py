1e8b7496c913c2b85be01a50510d3e8a16f91bbc
Matteo Ceccarello <mceccarello@unibz.it>
2022-05-27 11:55:30 +0200

    Bump PUFFINN version number

--- join-experiments/run.py
+++ join-experiments/run.py
@@ -853,7 +853,7 @@ DATASETS = {
 
 # Stores lazily the algorithm (i.e. as funcions to be called) along with their version
 ALGORITHMS = {
-    'PUFFINN':         lambda: (SubprocessAlgorithm(["build/PuffinnJoin"]), 1),
+    'PUFFINN':         lambda: (SubprocessAlgorithm(["build/PuffinnJoin"]), 2),
     # Local top-k baselines
     'BruteForceLocal': lambda: (BruteForceLocal(),                          1),
     'faiss-HNSW':      lambda: (FaissHNSW(),                                1),
