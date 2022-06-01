fix off-by-one in random-difficult.

join-experiments/run.py
@@ -784,18 +784,19 @@ def random_difficult(out_fn, n, d, k):
         return out_fn
     
     assert d % 3 == 0
-    assert n % k == 0
+    assert n % (k + 1) == 0
 
     D = d // 3
     # the base set of random vectors, expected unit length
-    X = np.random.normal(scale=1/D, size=(n//k, d))
+    np.random.seed(1)
+    X = np.random.normal(scale=1/D, size=(n// (k + 1), d))
     
     Y = np.zeros((n, d))
     for i, x in enumerate(X):
-        Y[k * i] = x
-        for j in range(1, k):
+        Y[(k + 1) * i] = x
+        for j in range(1, k + 1):
             # k random vectors at expected distance \sqrt{1/3} of x
-            Y[k * i + j] = np.concatenate((x[: 2 * D], np.random.normal(scale=1/D, size=D)))
+            Y[(k + 1) * i + j] = np.concatenate((x[: 2 * D], np.random.normal(scale=1/D, size=D)))
     X = X.astype(np.float32)
     write_dense(out_fn, X)
     return out_fn
@@ -1059,7 +1060,7 @@ DATASETS = {
     'DeepImage': lambda: deep_image(os.path.join(DATASET_DIR, 'deep_image.hdf5')),
     'NYTimes': lambda: nytimes(os.path.join(DATASET_DIR, 'nytimes.hdf5'), 256),
     'random-float-10k': lambda: random_float(os.path.join(DATASET_DIR, 'random-float-10k.hdf5' ), 20, 10000, 100),
-    'random-difficult': lambda: random_difficult(os.path.join(DATASET_DIR, 'random-float-difficult.hdf5' ), 1000000, 150, 10)
+    'random-difficult': lambda: random_difficult(os.path.join(DATASET_DIR, 'random-float-difficult.hdf5' ), 1100000, 150, 10)
 }
 
 # Stores lazily the algorithm (i.e. as funcions to be called) along with their version